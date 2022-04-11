2c3a2f39942ddcab8567accbb87aad7a565052a8
iskanderAB <iskanderabasi@gmail.com>
2022-04-11 13:11:06 +0100

    dom

---
+++ index.js
@@ -0,0 +1,142 @@
+// [] variables
+
+// var a = 10 ; 
+// a = "iskander";
+// console.log( a );
+
+// var a = "hello"; 
+
+// console.log(a);
+
+
+// let b  = "hello isg";
+// b = 20 ; 
+
+// console.log(b);
+
+// const pi = 3.14;
+
+
+// [] arrays 
+
+// const arr = [1,2,3,"hello","A"];
+ 
+
+
+// arr[0] = "test";
+                  
+// arr = "hello";
+
+// console.log(arr)
+
+
+// [] if
+
+// if ( 4 > 5 && 10> 2 ) { 
+//     console.log("true");
+// }else if ( 20 === "20" ){
+//     console.log("true 2")
+// }else{
+//     console.log("false");
+// }
+
+
+// [] loop
+
+// for(let i = 0,j=10; i<10,j>0 ; i++,j--){
+//     console.log("value i =>",i);
+//     console.log("value j =>",j);
+// }
+
+/*1*/
+
+
+// for (let i =0 ; i<tab.length ; i++){
+//     console.log(tab[i]);
+// }
+
+/*2*/
+// for(let element in tab ){
+//     console.log(tab[element]);
+// }
+
+// for(let element of tab ){
+//     console.log(element);
+// }
+
+
+// [] map
+// const tab = ["a","b","c"];
+
+// const newTab = tab.map(function(value,index){
+//     if (index === 0 )
+//         return "isg"
+//     return value;
+
+    
+// })
+// console.log(newTab);
+
+// const tabOfNumber = [1,-10,6,9,0,3,-3,9,8];
+// tabOfNumber.unshift(1000);
+// tabOfNumber.sort(function(a,b) {
+//     return a-b;
+// });
+
+
+
+// console.log(sum(4,5))
+//normale
+// function sum (a,b){
+//     return a+ b ;
+// }
+// console.log(test)
+// let test = 0 ; 
+
+
+// const mult = (a,b) => { 
+//     return a*b;
+// }
+
+
+
+// console.log(mult(5,4));
+
+
+
+
+// const tabOfNumber = [1,-10,6,9,0,3,-3,9,-0.5,8];
+
+// const ex1 = () => { 
+
+//     tabOfNumber.sort((a,b)=> {
+//        return a-b
+//     });
+//     console.log(tabOfNumber);
+//     for( let i in tabOfNumber ){
+//         if(tabOfNumber[i]=== 0){
+//             const right = tabOfNumber[+i+1]
+//             const left = tabOfNumber[i-1];
+//             if(Math.abs(left) > Math.abs(right) ){
+//                 console.log("the best is => ",right)
+//             }
+//             else{
+//                 console.log("the best is => ",left )
+//             }
+//         }
+//     }
+// }
+// ex1();
+
+
+
+
+
+
+
+
+
+// [] filter
+
+
+// [] reduce   
